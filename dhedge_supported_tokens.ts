79eee345bfc52862f8c88147fac83a67a7199b9c
mcmoodoo <hi@unpin.co>
2025-02-08 02:16:49 +0800

    hardcoded list of tokens added

---
+++ dhedge_supported_tokens.ts
@@ -0,0 +1,29 @@
+export const tokens = {
+  "tokens": [
+    "AAVE",
+    "ARB",
+    "BAL",
+    "CRV",
+    "GMX",
+    "LINK",
+    "sgETH",
+    "SPELL",
+    "SUSHI",
+    "tBTC",
+    "UNI",
+    "WBTC",
+    "Compound USDC",
+    "Compound WETH",
+    "BTCBEAR1X",
+    "BTCBULL3X",
+    "ETHBULL3X",
+    "DAI",
+    "FRAX",
+    "MIM",
+    "USDC",
+    "USDCe",
+    "USDT",
+    "wOETH",
+    "wstETH"
+  ]
+}
